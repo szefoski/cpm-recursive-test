4cce51a8e74323a167185e6e6ea98dd110d7ca45
szefoski <szefoski@gmail.com>
2025-12-22 15:07:13 +0100

    Format CMake comments for consistency
    - Reflow long comment lines in CMakeLists.txt
    - Consolidate CPMSetup.cmake header comment to single line

--- cmake/CPMSetup.cmake
+++ cmake/CPMSetup.cmake
@@ -1,5 +1,4 @@
-# CPM Setup Helper
-# Include this file from subprojects to ensure CPM is available
+# CPM Setup Helper Include this file from subprojects to ensure CPM is available
 
 if(NOT COMMAND CPMAddPackage)
   # Use vendored CPM.cmake from root project
